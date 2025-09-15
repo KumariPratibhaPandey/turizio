ad7d7b1c7fa3ad6dcdc7dfe580088a0c572556b5
KumariPratibhaPandey <kumaripratibhapandey0@gmail.com>
2025-09-15 20:18:28 +0530

    Initial commit of tourizio project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,21 @@ import { HomeComponent } from './home/home.component';
 import { DestinationsComponent } from './destinations/destinations.component';
 import { BookTourComponent } from './book-tour/book-tour.component';
 import { ContactComponent } from './contact/contact.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'destinations', component: DestinationsComponent },
   { path: 'book-tour', component: BookTourComponent },
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+
+  // Wildcard route for 404 handling
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
